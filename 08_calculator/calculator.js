75062b714e2e03e036f5ec972ab038a644b9921d
Omar S <sakho.omard@gmail.com>
2024-07-18 14:05:59 +0000

    meow

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b 
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  if(a == undefined || a == 0){
+    return 0
+  }else{
+    return a.reduce((accum,curr) => Number(accum) + Number(curr)) 
+  }
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  if(a == undefined || a == 0){
+    return 0
+  }else{
+    return a.reduce((accum,curr) => Number(accum) * Number(curr)) 
+  }
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	if(a == undefined || a == 0){
+    return 0
+  }else{
+    let multi = 1; 
+    for (i = 0; i < b ; i++){
+    multi *= a
+  }
+  return multi
+  }
 };
 
 const factorial = function() {
